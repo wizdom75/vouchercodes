2b99b4016f89ca0a58f1f6ba14cef76af20ed0ab
wizdom75 <pmn@outlook.com>
2019-11-23 22:46:58 +0000

    Another bug squashed

--- database/migrations/2019_11_23_195553_fix_some_colums.php
+++ database/migrations/2019_11_23_195553_fix_some_colums.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class FixSomeColums extends Migration
 {
@@ -13,6 +14,7 @@ class FixSomeColums extends Migration
      */
     public function up()
     {
+        $this->registerEnumWithDoctrine();
         Schema::table('feed_settings', function (Blueprint $table) {
             $table->renameColumn('promo_col', 'promo_id_col');
         });
@@ -28,6 +30,19 @@ class FixSomeColums extends Migration
      */
     public function down()
     {
-        //
+        $this->registerEnumWithDoctrine();
+        Schema::table('feed_settings', function (Blueprint $table) {
+            $table->renameColumn('promo_col', 'promo_id_col');
+        });
+        Schema::table('vouchers', function (Blueprint $table) {
+            $table->text('category_slug')->change();
+        });
+    }
+
+    private function registerEnumWithDoctrine()
+    {
+        DB::getDoctrineSchemaManager()
+            ->getDatabasePlatform()
+            ->registerDoctrineTypeMapping('enum', 'string');
     }
 }
