cd80e168c29457b7d8db7a9740f1ee0bf2a46986
Peter Ncube <pmn@outlook.com>
2021-04-29 23:31:52 +0100

    Check feed settings

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -234,18 +234,16 @@ class FeedController extends Controller
                 //Skip headers in CSV file
             }else{ 
                 //Skip voucher if it is already in database
-                
+                var_dump($data);
                 if(Voucher::where('promo_id', $data[$feedSettings->promo_col])->first()){
                     continue;
                 }
-                var_dump($data);
-                continue;
-                $valid_from = date('Y-m-d H:i:s', strtotime(str_replace('-', '/',  mb_convert_encoding($data[$feedSettings->valid_from_col], 'UTF-8', 'UTF-8'))));
+                $valid_from = date('Y-m-d H:i:s', strtotime(str_replace('-', '/',  $data[$feedSettings->valid_from_col])));
                 
                 if($valid_from < '1970-01-01 00:00:01' || $valid_from > '2038-01-19 03:14:07'){
                     $valid_from = '1970-01-01 00:00:10';
                 }
-                $valid_to = date('Y-m-d H:i:s', strtotime(str_replace('-', '/',  mb_convert_encoding($data[$feedSettings->valid_to_col], 'UTF-8', 'UTF-8'))));
+                $valid_to = date('Y-m-d H:i:s', strtotime(str_replace('-', '/',  $data[$feedSettings->valid_to_col])));
                 if($valid_to < '1970-01-01 00:00:01' || $valid_to > '2038-01-19 03:14:07'){
                     $valid_to = '2030-01-01 01:00:10';
                 }
